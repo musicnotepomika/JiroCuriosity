98007eee29bb0ff6666339b776ea42a3dc0448d7
musicnotepomika <morganndavid04@gmail.com>
2025-09-16 20:29:03 +0200

    the DB creation annoys me

--- curiosity.sql
+++ curiosity.sql
@@ -1,46 +1,46 @@
 --
--- Database `dbCuriosity`
+-- Database dbCuriosity
 --
-DROP DATABASE IF EXISTS `dbCuriosity`;
-CREATE DATABASE `dbCuriosity`;
-USE `dbCuriosity`;
+DROP DATABASE IF EXISTS dbCuriosity;
+CREATE DATABASE dbCuriosity;
+USE dbCuriosity;
 ---------------------------
 --
--- `species` table structure
+-- species table structure
 --
-CREATE TABLE IF NOT EXISTS `species`(
-    `id` INT(3) NOT NULL,
-    `name` VARCHAR(25) NOT NULL,
-    PRIMARY KEY(`id`)
+CREATE TABLE IF NOT EXISTS species(
+    id INT(3) NOT NULL,
+    name VARCHAR(25) NOT NULL,
+    PRIMARY KEY(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 --
--- `characters` table structure
+-- characters table structure
 --
-CREATE TABLE IF NOT EXISTS `characters`(
-    `id` VARCHAR(30) NOT NULL,
-    `fisrtName` VARCHAR(25) DEFAULT NULL,
-    `lastName` VARCHAR(25) DEFAULT NULL,
-    `speciesId` INT(3) NOT NULL,
-    `description` LONGTEXT DEFAULT NULL,
-    `deaths` INT(5) DEFAULT NULL,
-    `originId` INT(3) DEFAULT NULL,
-    PRIMARY KEY(`id`)
+CREATE TABLE IF NOT EXISTS characters(
+    id VARCHAR(30) NOT NULL,
+    fisrtName VARCHAR(25) DEFAULT NULL,
+    lastName VARCHAR(25) DEFAULT NULL,
+    speciesId INT(3) NOT NULL,
+    description LONGTEXT DEFAULT NULL,
+    deaths INT(5) DEFAULT NULL,
+    originId INT(3) DEFAULT NULL,
+    PRIMARY KEY(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 --
--- `origin` table structure
+-- origin table structure
 --
-CREATE TABLE IF NOT EXISTS `origin`(
-    `id` INT(3),
-    `name` VARCHAR(25),
-    `mediaGenre` VARCHAR(25),
-    PRIMARY KEY(`id`)
+CREATE TABLE IF NOT EXISTS origin(
+    id INT(3),
+    name VARCHAR(25),
+    mediaGenre VARCHAR(25),
+    PRIMARY KEY(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 --
 -- foreign keys
 --
-ALTER TABLE `characters`
-ADD CONSTRAINT `characterSpecies` FOREIGN KEY (`speciesId`) REFERENCES `species`(`id`),
-ADD CONSTRAINT `characterOrigin` FOREIGN KEY (`originId`) REFERENCES `origin`(`id`);
+ALTER TABLE characters
+ADD CONSTRAINT characterSpecies FOREIGN KEY (speciesId) REFERENCES species(id),
+ADD CONSTRAINT characterOrigin FOREIGN KEY (originId) REFERENCES origin(id);
 --
 -- users creation
 --
--- index.php
+++ index.php
@@ -10,8 +10,13 @@
     <body>
         <?php include('header.php'); ?>
         <h2>Home page</h2>
-        <p>Welcome to the Curiosity Project website.</p><br>
-        <p>This website is still under construction.</p>
+        <p>Welcome to the Curiosity Project website.</p>
+        <h3>About Curiosity</h3>
+        <p>The Curiosity Project is a project made by Jiro Segoe to make a machine that can create life.</p>
+        <a href="about.php">More...</a>
+        <br><br><h3>The Creator</h3>
+        <p>Jiro Segoe, 21 years old, curious engineer since childhood.</p>
+        <p>He is the most intelligent member of the Segoe clan.</p>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     </body>
 </html>
