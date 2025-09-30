011d129b6e29caa616b1b3b58a3e65ff48431522
musicnotepomika <morganndavid04@gmail.com>
2025-09-30 20:27:34 +0200

    added new getSelected function

--- characters.php
+++ characters.php
@@ -5,3 +5,9 @@ function getAll(){
     $stmt=$db->query("SELECT characters.id AS characterId, firstName, lastName, gender, description, deaths, imageURL, name FROM characters INNER JOIN species ON species.id=characters.speciesId");
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
+function getSelected($selectedId){
+    $db=connect();
+    $stmt=$db->query("SELECT characters.id AS characterId, firstName, lastName, gender, description, deaths, imageURL, name FROM characters INNER JOIN species ON species.id=characters.speciesId WHERE characterId=",$selectedId);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
