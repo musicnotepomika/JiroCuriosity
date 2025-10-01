3e6f98317691b46359ddfae30f74f6eb0ce4f47b
musicnotepomika <morganndavid04@gmail.com>
2025-10-01 20:28:31 +0200

    tried to use the function to get the selected value in the select but stumbled into a new problem :skull:

--- characterInfo.php
+++ characterInfo.php
@@ -10,24 +10,31 @@
     <body>
         <?php include("header.php");
         require("characters.php");
-        $characters=getAll();?>
+        $characters=getAll();
+        $selectedChar=null;
+        $formValue=$_POST["charSelect"];?>
 
-        <h2><?= htmlspecialchars($character['characterId']) ?></h2>
-        <form action="#" method="post">
-            <select name="characterSelect" id="charSelect">
+        <h2><?= htmlspecialchars($selectedChar['characterId']) ?></h2>
+        <form action="#" method="post" name="selectChar">
+            <select name="characterSelect" name="charSelect">
                 <option value="">--Please choose a character--</option>
                 <?php foreach($characters as $character): ?>
                 <option value="(<?= htmlspecialchars($character['characterId'])?>">
                     <?= htmlspecialchars($character['characterId']) ?>
                 </option>
-                <?php endforeach ?>
+                <?php if(isset($formValue)==true && getSelected($character['characterId'])==$formValue){
+                    $selectedChar=$character;
+                }
+                endforeach ?>
             </select>
             <button type="submit">Select</button>
         </form>
-        <img src="img/<?= htmlspecialchars($character['imageURL'])?>" alt="" class="infoPic">
-        <h2>Name : <?= htmlspecialchars($character['firstName'])?> <?=htmlspecialchars($character['lastName'])?></h2>
-        <h2>Gender : <?= htmlspecialchars($character['gender'])?></h2>
-        <h2>Species : <?= htmlspecialchars($character['name'])?></h2>
-        <p><?= htmlspecialchars($character['description'])?></p>
+        <img src="img/<?= htmlspecialchars($selectedChar['imageURL'])?>" alt="" class="infoPic">
+        <h2>Name : <?= htmlspecialchars($selectedChar['firstName'])?> <?=htmlspecialchars($selectedChar['lastName'])?></h2>
+        <h2>Gender : <?= htmlspecialchars($selectedChar['gender'])?></h2>
+        <h2>Species : <?= htmlspecialchars($selectedChar['name'])?></h2>
+        <p><?= htmlspecialchars($selectedChar['description'])?></p>
+        <?php var_dump($selectedChar); 
+        var_dump($formValue);?>
     </body>
 </html>
